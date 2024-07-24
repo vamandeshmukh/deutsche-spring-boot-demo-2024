0a44a1dbecbd7035bbcbf95969780d74615f5b1a
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-07-24 16:06:28 +0530

    spring-boot-demo

--- src/main/java/com/db/demo/controller/HelloController.java
+++ src/main/java/com/db/demo/controller/HelloController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+//	view output at: 
+//  http://localhost:8080/hello
+	
 	@GetMapping("hello")
 	public String hello() {
 		System.out.println("Hello");
